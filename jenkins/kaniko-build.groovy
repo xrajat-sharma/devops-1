7cab8478158a93722ca575832bf7708484cefa3e
Avinash Kumar Singh <65069994+avinash-0-singh@users.noreply.github.com>
2020-07-04 16:19:58 +0530

    Update kaniko-build.groovy

--- jenkins/kaniko-build.groovy
+++ jenkins/kaniko-build.groovy
@@ -20,7 +20,7 @@ spec:
     tty: true
     volumeMounts:
       - name: jenkins-docker-cfg
-        mountPath: /kaniko/.docker/config.json
+        mountPath: /kaniko/.docker
   volumes:
   - name: jenkins-docker-cfg
     projected:
